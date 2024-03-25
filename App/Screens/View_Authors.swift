531559c80e52f8b8b21e443ba5d0c89b33e89da0
Gritsaenko Alexandra <avgritsaenko_1@edu.hse.ru>
2024-03-25 23:19:36 +0300

    screens

---
+++ App/Screens/View_Authors.swift
@@ -0,0 +1,18 @@
+//
+//  View_Authors.swift
+//  App
+//
+//  Created by Александра Грицаенко on 25.03.2024.
+//
+
+import SwiftUI
+
+struct View_Authors: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    View_Authors()
+}
---
+++ App/Screens/View_MainScreen.swift
@@ -0,0 +1,104 @@
+//
+//  View_MainScreen.swift
+//  App
+//
+//  Created by Александра Грицаенко on 25.03.2024.
+//
+
+import SwiftUI
+
+struct View_MainScreen: View {
+    var body: some View {
+        
+        ZStack {
+            Color("BackgroundColor")
+                .ignoresSafeArea()
+            VStack {
+                Text("Game Name")
+                    .padding()
+                    .frame(maxWidth: 350)
+                    .background(Color("ButtonColor"))
+                    .font(.custom("AvenirNext-Bold", size: 38))
+                    .border(Color("ButtonBorder"), width: 5)
+                    .cornerRadius(10)
+                    .foregroundStyle(.white)
+                    .padding(.bottom, 35)
+                
+                Button {
+                    print("Start")
+                } label: {
+                    Text("Новая игра")
+                        .padding()
+                        .frame(maxWidth: 253)
+                        .background(Color("ButtonColor"))
+                        .font(.custom("AvenirNext-Bold", size: 38))
+                        .border(Color("ButtonBorder"), width: 5)
+                        .cornerRadius(10)
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.white)
+                }
+                
+                Button {
+                    print()
+                } label: {
+                    Text("Загрузить")
+                        .padding()
+                        .frame(maxWidth: 253)
+                        .background(Color("ButtonColor"))
+                        .font(.custom("AvenirNext-Bold", size: 38))
+                        .border(Color("ButtonBorder"), width: 5)
+                        .cornerRadius(10)
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.white)
+                }
+                
+                Button {
+                    print()
+                } label: {
+                    Text("Настройки")
+                        .padding()
+                        .frame(maxWidth: 253)
+                        .background(Color("ButtonColor"))
+                        .font(.custom("AvenirNext-Bold", size: 38))
+                        .border(Color("ButtonBorder"), width: 5)
+                        .cornerRadius(10)
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.white)
+                }
+                
+                Button {
+                    print()
+                } label: {
+                    Text("Об авторах")
+                        .padding()
+                        .frame(maxWidth: 253)
+                        .background(Color("ButtonColor"))
+                        .font(.custom("AvenirNext-Bold", size: 38))
+                        .border(Color("ButtonBorder"), width: 5)
+                        .cornerRadius(10)
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.white)
+                }
+                
+                Button {
+                    print()
+                } label: {
+                    Text("Выход")
+                        .padding()
+                        .frame(maxWidth: 253)
+                        .background(Color("ButtonColor"))
+                        .font(.custom("AvenirNext-Bold", size: 38))
+                        .border(Color("ButtonBorder"), width: 5)
+                        .cornerRadius(10)
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.white)
+                }
+            }
+        }
+    }
+}
+
+
+#Preview {
+    View_MainScreen()
+}
